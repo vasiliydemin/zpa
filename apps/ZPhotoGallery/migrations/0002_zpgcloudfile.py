a371a36cf14e40914a4e471c2221fe91bc8077b1
ambler <vasiliydemin@gmail.com>
2018-10-28 10:16:24 +0300

    commit

--- apps/ZPhotoGallery/migrations/0002_zpgcloudfile.py
+++ apps/ZPhotoGallery/migrations/0002_zpgcloudfile.py
@@ -6,7 +6,6 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ZPhotoGallery', '0001_initial'),
     ]
 
     operations = [
@@ -17,8 +16,8 @@ class Migration(migrations.Migration):
                 ('filename', models.CharField(blank=True, max_length=128, null=True)),
                 ('preview', models.CharField(blank=True, max_length=512, null=True)),
                 ('path', models.CharField(blank=True, max_length=512, null=True)),
-                ('hash', models.CharField(blank=True, max_length=254, null=True)),
-                ('minme_type', models.CharField(blank=True, max_length=56, null=True)),
+                ('hash', models.CharField(blank=True, max_length=254, null=True, unique=True)),
+                ('mime_type', models.CharField(blank=True, max_length=56, null=True)),
                 ('size', models.FloatField(blank=True, null=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('last_upd', models.DateTimeField(auto_now=True)),
